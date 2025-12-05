92e57aee960ccd71fcfb6d3d81344045fa858d56
cmankowski1992-glitch <cmankowski1992@gmail.com>
2025-12-05 15:20:30 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_10_ef6404_nand user_project (
+  tt_um_christina_mankowski_nand user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
