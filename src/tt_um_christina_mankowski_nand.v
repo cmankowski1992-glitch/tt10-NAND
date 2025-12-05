8e62af838f0f02310f7c9fcc7f5e40eff6b1dcc9
cmankowski1992-glitch <cmankowski1992@gmail.com>
2025-12-05 15:14:19 -0500

    Update and rename tt_10_ef6404_nand.v to tt_um_christina_mankowski_nand.v

--- src/tt_um_christina_mankowski_nand.v
+++ src/tt_um_christina_mankowski_nand.v
@@ -4,7 +4,7 @@
  */
 
 `default_nettype none
-module tt_10_ef6404_nand (
+module tt_um_christina_mankowski_nand(
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
